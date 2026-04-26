8d8bff80e94e0b536a85627b22a60ba0272dea1e
AnhLove <tuankaito6005@gmail.com>
2026-04-26 09:32:14 +0700

    Initial commit: BoxingHub Spring Boot setup

---
+++ src/main/java/com/boxinghub/boxinghub/BoxinghubApplication.java
@@ -0,0 +1,13 @@
+package com.boxinghub.boxinghub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoxinghubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoxinghubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boxinghub/boxinghub/BoxinghubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boxinghub.boxinghub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoxinghubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
